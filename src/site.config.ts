0201c97d2e6e8f1f832a4808cb6cd520c13a05b7
quangthaictc <quangthaictc@gmail.com>
2026-03-09 09:45:57 +0700

    fix: Github Actions

--- src/site.config.ts
+++ src/site.config.ts
@@ -137,6 +137,27 @@ export const integ: IntegrationUserConfig = {
     options: {
       className: 'zoomable'
     }
+  },
+  // Comment system
+  waline: {
+    enable: false,
+    // Server service link
+    server: 'https://astro-theme-pure-waline.arthals.ink/',
+    // Show meta info for comments
+    showMeta: false,
+    // Refer https://waline.js.org/en/guide/features/emoji.html
+    emoji: ['bmoji', 'weibo'],
+    // Refer https://waline.js.org/en/reference/client/props.html
+    additionalConfigs: {
+      // search: false,
+      pageview: true,
+      comment: true,
+      locale: {
+        reaction0: 'Like',
+        placeholder: 'Welcome to comment. (Email to receive replies. Login is unnecessary)'
+      },
+      imageUploader: false
+    }
   }
 }
 
